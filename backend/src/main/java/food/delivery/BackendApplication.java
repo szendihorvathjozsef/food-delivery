ae594cb2d1c46e41bc87fd95cdaa23cc7706c37a
szendi-horvath.jozsef <szendi-horvath.jozsef@promera.hu>
2019-11-06 17:19:12 +0100

    WIP

--- backend/src/main/java/food/delivery/BackendApplication.java
+++ backend/src/main/java/food/delivery/BackendApplication.java
@@ -1,13 +1,26 @@
 package food.delivery;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
+    private final Logger log = LoggerFactory.getLogger(BackendApplication.class);
+
+    @Value("${spring.datasource.url}")
+    private String database;
+
     public static void main(String[] args) {
+        new BackendApplication().start();
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    public void start() {
+        log.info("DATABASE URL: {}", database);
     }
 
 }
