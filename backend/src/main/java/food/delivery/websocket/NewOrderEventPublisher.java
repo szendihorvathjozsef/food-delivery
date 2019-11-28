c0c1fefda93047683e8d243f0243babda77cb331
szendi-horvath.jozsef <szendi-horvath.jozsef@promera.hu>
2019-11-28 21:22:51 +0100

    websocket order finish

---
+++ backend/src/main/java/food/delivery/websocket/NewOrderEventPublisher.java
@@ -0,0 +1,23 @@
+package food.delivery.websocket;
+
+import food.delivery.services.dto.OrderDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author szendihorvathjozsef
+ */
+@Component
+public class NewOrderEventPublisher {
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    public void publish(OrderDTO orderDTO) {
+        NewOrderEvent newOrderEvent = new NewOrderEvent(this, orderDTO);
+        publisher.publishEvent(newOrderEvent);
+    }
+
+}
